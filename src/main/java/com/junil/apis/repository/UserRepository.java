a06db40137e5960af4becb4f623c9b1b81c193f2
nwearfd <wearfd031013@naver.com>
2020-12-14 13:54:22 +0900

    add: new file

---
+++ src/main/java/com/junil/apis/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.junil.apis.repository;
+
+import com.junil.apis.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
