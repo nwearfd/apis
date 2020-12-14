dc39ea8339fb160820dd3ceb7df79201c7ff600c
nwearfd <wearfd031013@naver.com>
2020-12-14 14:19:04 +0900

    add: new file

--- src/main/java/com/junil/apis/route/User.java
+++ src/main/java/com/junil/apis/route/User.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class User {
 
+
     @GetMapping("/user")
     public String getUsers(){
         return "users!";
---
+++ src/main/java/com/junil/apis/service/UserService.java
@@ -0,0 +1,15 @@
+package com.junil.apis.service;
+
+import com.junil.apis.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+}
